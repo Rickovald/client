558af4831a5e7f7ea8e6568fd0528405cab77214
Actina <rikonvald@gmail.com>
2022-07-25 18:02:10 +0300

    page main styles and presets update

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,9 +1,38 @@
 import s from './footer.module.sass'
+import tt from '../media/social/TikTok_white.svg'
+import vk from '../media/social/VK_white.svg'
+import inst from '../media/social/Instagram_white.svg'
 
 const Footer = () => {
   return (
     <div className={s.root}>
-      Footer
+      © 2022 All rights reserved
+      <div className={s.social}>
+        <a
+          className={s.href}
+          href="https://www.tiktok.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={tt} alt="tiktok" className={s.icon} />
+        </a>
+        <a
+          className={s.href}
+          href="https://vk.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={vk} alt="vk" className={s.icon} />
+        </a>
+        <a
+          className={s.href}
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={inst} alt="inst" className={s.icon} />
+        </a>
+      </div>
     </div>
   )
 }
--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -4,9 +4,16 @@ import s from './navigation.module.sass'
 const Navigation = () => {
   return (
     <div className={s.root}>
-      <NavLink to='/'>Main</NavLink>
-      <NavLink to='/about'>About</NavLink>
-      <NavLink to='/contacts'>Contacts</NavLink>
+      <div className={s.wrapper}>
+        <div className={s.logo}>
+          Logo
+        </div>
+        <div className={s.links}>
+          <NavLink className={s.link} to='/'>Main</NavLink>
+          <NavLink className={s.link} to='/about'>About</NavLink>
+          <NavLink className={s.link} to='/contacts'>Contacts</NavLink>
+        </div>
+      </div>
     </div>
   )
 }
