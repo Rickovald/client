2afc320ad28354996a9c7fcadb93b084eed66b79
Actina <rikonvald@gmail.com>
2022-08-19 02:39:31 +0300

    plop v3 template upd

--- plopfile.js
+++ plopfile.js
@@ -1,43 +1,64 @@
-
 module.exports = function (plop) {
-  plop.setGenerator('module', {
-    description: 'Generate new tsx react FC module',
-    prompts: [
-      {
-        type: 'input',
-        name: 'title',
-        message: 'Name of FC?'
-      }
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'src/{{properCase title}}/{{properCase title}}.tsx',
-        templateFile: '_templates/functionalComponent.hbs'
-      },
-      {
-        type: 'add',
-        path: 'src/{{properCase title}}/{{camelCase title}}.module.sass',
-        templateFile: '_templates/styleSass.hbs'
-      },
-      {
-        type: 'append',
-        path: 'src/App.tsx',
-        pattern: /(\/\/ COMPONENT IMPORTS)/g,
-        template: 'import {{properCase title}} from \'./{{properCase title}}/{{properCase title}}\''
-      },
-      {
-        type: 'append',
-        path: 'src/App.tsx',
-        pattern: /\{\/\* COMPONENT ROUTES \*\/\}$/gm,
-        template: '          <Route path=\'/{{snakeCase title}}\' element={<{{properCase title}} />}/>'
-      },
-      {
-        type: 'append',
-        path: 'src/Navigation/Navigation.tsx',
-        pattern: /\{\/\* COMPONENT LINKS \*\/\}$/gm,
-        template: '          <NavLink className={s.link} to=\'{{snakeCase title}}\'>{{properCase title}}</NavLink>'
-      }
-    ]
-  })
-}
+    plop.setGenerator('page', {
+        description: 'Generate new tsx react FC page',
+        prompts: [
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Name of FC page?'
+            }
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: 'src/{{properCase title}}/{{properCase title}}.tsx',
+                templateFile: '_templates/functionalComponent.hbs'
+            },
+            {
+                type: 'add',
+                path: 'src/{{properCase title}}/{{camelCase title}}.module.sass',
+                templateFile: '_templates/styleSass.hbs'
+            },
+            {
+                type: 'append',
+                path: 'src/App.tsx',
+                pattern: /(\/\/ COMPONENT IMPORTS)/g,
+                template: 'import {{properCase title}} from \'./{{properCase title}}/{{properCase title}}\''
+            },
+            {
+                type: 'append',
+                path: 'src/App.tsx',
+                pattern: /\{\/\* COMPONENT ROUTES \*\/\}$/gm,
+                template: '          <Route path=\'/{{snakeCase title}}\' element={<{{properCase title}} />}/>'
+            },
+            {
+                type: 'append',
+                path: 'src/Navigation/Navigation.tsx',
+                pattern: /\{\/\* COMPONENT LINKS \*\/\}$/gm,
+                template: '          <NavLink className={s.link} to=\'{{snakeCase title}}\'>{{properCase title}}</NavLink>'
+            }
+        ]
+    });
+    plop.setGenerator('component', {
+        description: 'Generate new tsx react FC component',
+        prompts: [
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Name of FC?'
+            }
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: 'src/components/{{properCase title}}/{{properCase title}}.tsx',
+                templateFile: '_templates/functionalComponent.hbs'
+            },
+            {
+                type: 'add',
+                path: 'src/components/{{properCase title}}/{{camelCase title}}.module.sass',
+                templateFile: '_templates/styleSass.hbs'
+            }
+        ]
+    });
+};
