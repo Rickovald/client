784d1df4e4a815e44ead9552a016d4cc0b8426b9
Rikovald <rikonvald@gmail.com>
2024-06-27 10:18:57 +0300

    helmet and fonts fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 // COMPONENT IMPORTS
 import { Footer } from 'widgets/Footer';
@@ -11,33 +11,35 @@ import { Contacts } from 'pages/Contacts';
 
 const App = () => {
     return (
-        <div className='App'>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>Page title</title>
-                <link rel="canonical" href="http://mysite.com/example" />
-            </Helmet>
-            <Router>
-                <Navigation />
-                <Routes>
-                    <Route
-                        path='/'
-                        element={<Main />}
-                    />
-                    <Route
-                        path='/about'
-                        element={<About />}
-                    />
-                    <Route
-                        path='/contacts'
-                        element={<Contacts />}
-                    />
-                    {/* COMPONENT ROUTES */}
-                    <Route path='*' element={<Error404 />} />
-                </Routes>
-                <Footer />
-            </Router>
-        </div>
+        <HelmetProvider>
+            <div className='App'>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Page title</title>
+                    <link rel="canonical" href="http://mysite.com/example" />
+                </Helmet>
+                <Router>
+                    <Navigation />
+                    <Routes>
+                        <Route
+                            path='/'
+                            element={<Main />}
+                        />
+                        <Route
+                            path='/about'
+                            element={<About />}
+                        />
+                        <Route
+                            path='/contacts'
+                            element={<Contacts />}
+                        />
+                        {/* COMPONENT ROUTES */}
+                        <Route path='*' element={<Error404 />} />
+                    </Routes>
+                    <Footer />
+                </Router>
+            </div>
+        </HelmetProvider>
     );
 };
 
